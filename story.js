e48801328475711310bc6c720dd4e4d2e65a42af
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-09-24 13:04:08 +0300

    first commit

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = 'khalid';
+let age = 25;
+let hobby = 'football';
+
+console.log(`Mr./Mrs. ${name}, in ${age} years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`)
